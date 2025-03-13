51370e89cd000dfd0c2a3ecb04138c10b8ce0a3c
cunpu1411 <117759077+cunpu1411@users.noreply.github.com>
2025-03-13 22:57:02 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import { connect } from "mongoose";
 import taskRoutes from "./routes/tasks";
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
