de3b61fa4fbc5f75d4ea721dd522a44164050421
William Ma <mqcuong1603@gmail.com>
2025-03-18 13:00:27 +0700

    set up somgthing

--- backend/server.js
+++ backend/server.js
@@ -25,5 +25,5 @@ app.get("/health", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on: http://localhost:${PORT}`);
 });
